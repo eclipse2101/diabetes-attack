276a6887caca46fbb6cb6313412af4814e5c1913
explosiveseccc <rdericieux25@aoiths.org>
2023-12-18 12:40:11 -0500

    e

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -39,17 +39,17 @@ public class playerController : MonoBehaviour
         //// Borders////
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
         if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
         if (transform.position.z < -zRange)
         {
-            transform.position = new Vector3( transform.position.x, transform.position.y, -zRange);
+            transform.position = new Vector3( transform.position.x, transform.position.y, zRange);
         }
 
         if (transform.position.z > zRange)
